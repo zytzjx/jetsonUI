0f72a07a0c9a561a43250acb46bde8a9efb27bca
zytzjx <zytzjx@sina.com>
2020-05-16 21:31:00 -0700

    change raspi to Jetson

--- config.sh
+++ config.sh
@@ -15,23 +15,32 @@ then
 fi
 
 sudo apt-get update && sudo apt-get upgrade
-#sudo apt -y install xscreensaver
-sudo apt-get -y git curl
-sudo apt-get -y install qt5-default pyqt5-dev pyqt5-dev-tools
-#sudo apt-get -y install matchbox-keyboard
-#sudo apt -y install python3-opencv #python3-opencv-apps
-sudo apt-get -y install libatlas-base-dev
-
-
-
-#auto remove no use
-sudo apt-get -y remove python-pygame
-sudo apt-get -y remove minecraft-pi
-sudo apt -y autoremove
-sudo apt-get -y purge wolfram-engine
 sudo apt-get -y purge libreoffice*
 sudo apt-get -y clean
 sudo apt-get -y autoremove
+
+sudo apt-get -y install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
+
+sudo apt-get -y install qt5-default pyqt5-dev pyqt5-dev-tools
+sudo apt-get -y install python3-matplotlib python3-numpy python3-pil python3-scipy python3-tk
+sudo apt-get -y install build-essential python3-dev cython3 python3-setuptools python3-pip python3-wheel python3-numpy python3-pytest python3-blosc python3-brotli python3-snappy python3-lz4 libz-dev libblosc-dev liblzma-dev liblz4-dev libzstd-dev libpng-dev libwebp-dev libbz2-dev libopenjp2-7-dev libjpeg-turbo8-dev libjxr-dev liblcms2-dev libcharls-dev libaec-dev libbrotli-dev libsnappy-dev libzopfli-dev libgif-dev libtiff-dev
+
+
+wget https://bootstrap.pypa.io/get-pip.py 
+python3 ./get-pip.py
+rm ./get-pip.py
+
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade Pillow
+python3 -m pip install --upgrade pyserial
+python3 -m pip install imagecodecs==2019.5.22
+python3 -m pip install scikit-image==0.16.2
+python3 -m pip install dataclasses
+
+
+##low is zero
+sudo apt-get update
+sudo apt-get install avahi-daemon avahi-discover libnss-mdns
 
 ##uponly
 sudo apt-get install libfakekey-dev libpng-dev libxft-dev autoconf libtool -y
@@ -41,30 +50,11 @@ cd matchbox-keyboard
 ./autogen.sh
 make
 sudo make install
-cd ~/Desktop/pyUI
+cd ~/Desktop/jetsonUI
+cp ./PSI.desktop ~/Desktop/
 
-sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
-sudo apt-get install build-essential cython3
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 ./get-pip.py
 
-python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade Pillow
-
-pip3 install paramiko
-pip3 install pyserial
-
-sudo apt-get install build-essential python3-dev cython3 python3-setuptools python3-pip python3-wheel python3-numpy python3-pytest python3-blosc python3-brotli python3-snappy python3-lz4 libz-dev libblosc-dev liblzma-dev liblz4-dev libzstd-dev libpng-dev libwebp-dev libbz2-dev libopenjp2-7-dev libjpeg-turbo8-dev libjxr-dev liblcms2-dev libcharls-dev libaec-dev libbrotli-dev libsnappy-dev libzopfli-dev libgif-dev libtiff-dev
-pip install imagecodecs==2019.5.22
-pip install scikit-image==0.16.2
 ##end uponly
-
-#### downonly
-#pip3 install scikit-image
-#pip3 install scipy
-##end downonly
-
-
 
 #https://blog.csdn.net/qq1187239259/article/details/80022272
 #setting up raspi share network
