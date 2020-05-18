c698ec312d24f541424c74cbffb31c20ebe498d7
Chris Chen <cchen@Futuredial.com>
2020-05-17 20:21:19 -0700

    testGS update

--- testGS.py
+++ testGS.py
@@ -38,10 +38,10 @@ def gstreamer_pipeline_2(
     flip_method=0,
 ):
     return (
-        "nvarguscamerasrc sensor-id=1 ! "
+        "nvarguscamerasrc sensor-id=0 ! "
         "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, format=(string)NV12, framerate=(fraction)%d/1 ! "
         "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)I420 ! appsink"
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)RGBA ! appsink"
         # "videoconvert ! video/x-raw, format=(string)BGR ! appsink"
         % (
             capture_width,
@@ -53,9 +53,65 @@ def gstreamer_pipeline_2(
         )
     )
 
+def gstreamer_pipeline_3(
+    capture_width=3280,
+    capture_height=2464,
+    display_width=3280,
+    display_height=2464,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=0 ! "
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv ! "
+        "video/x-raw, format=(string)I420 ! appsink"
+        # "videoconvert ! video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+        )
+    )
+
+def gstreamer_pipeline_4(
+    capture_width=3280,
+    capture_height=2464,
+    display_width=3280,
+    display_height=2464,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "videotestsrc ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)I420, framerate=(fraction)%d/1 ! "
+        "appsink "
+        # "video/x-raw, format=(string)I420 ! appsink"
+        # "videoconvert ! video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+        )
+    )
+
+def gstreamer_pipeline_5(
+    capture_width=3280,
+    capture_height=2464,
+    display_width=3280,
+    display_height=2464,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "udpsrc port=5000 ! application/x-rtp,encoding-name=JPEG,payload=26 ! rtpjpegdepay !"
+        "jpegdec ! video/x-raw, format=(string)I420 ! appsink"
+    )
+
+
 def save_image():
     print(gstreamer_pipeline(flip_method=0, capture_height=2464, capture_width=3280, framerate=21))
-    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0, capture_height=2464, capture_width=3264, display_height=2464, display_width=3264, framerate=21), cv2.CAP_GSTREAMER)
+    cap = cv2.VideoCapture(gstreamer_pipeline_5(flip_method=0, capture_height=2464, capture_width=3264, display_height=2464, display_width=3264, framerate=21), cv2.CAP_GSTREAMER)
     delay=100
     while(cap.isOpened()):
         ret_val, img = cap.read()
